22204c682e45dd808d538345874c5425946f7e4f
julianbetancourt <julianbetancourt10@gmail.com>
2016-02-26 17:33:40 -0500

    Añadir variables y funciones de obtenerDuracion, obtenerEstado, obtenerNotificacion

---
+++ functions.js
@@ -0,0 +1,47 @@
+var corriendo = false;
+var transcurrido = 0; //ms
+var transcurridoSesion = 0; //ms
+var minuto = 60 * 100; //ms
+var duracionTrabajo = 1 * minuto; //ms
+var duracionBreak = 5 * minuto; //ms
+var estadoActual = true; //true = trabajo, false = break
+var sesionesCompletas = 0;
+var tiempoInicio;
+var relojID;
+var inicioTrabajo = new Audio('http://oringz.com/ringtone/communication-channel/sounds-917-communication-channel/?download');
+var finalTrabajo = new Audio('http://oringz.com/ringtone/hold-your-horses/sounds-999-hold-your-horses/?download');
+
+Notification.requestPermission();
+
+var obtenerDuracion = function (estadoActual) {
+  //si estadoActual es trabajo
+  if (estadoActual) {
+    return duracionTrabajo;
+  } else { // si no
+    return duracionBreak;
+  }
+}
+
+var obtenerEstado = function (estadoActual, corriendo) {
+  var output = "";
+
+  //si estadoActual = true (trabajo)
+  if (estadoActual) {
+    output = 'Trabajando';
+  } else { //si estadoActual = false(break)
+    output = 'En break';
+  }
+  //si corriendo = false
+  if (!corriendo) {
+    ouput = 'Pausado';
+  }
+  return output;
+}
+
+function obtenerNotificacion(cuerpo, icono, titulo) {
+  var options = {
+    body: cuerpo,
+    icon: icono
+  }
+  var n = new Notification(titulo, options);
+}
